87f8048810cd20029807a6fb6fcfb26c9ebe5a83
Basma <basmaelandalosy5@gmail.com>
2024-08-01 09:46:55 +0100

    Suprimer une tache

--- ListToDo/script.js
+++ ListToDo/script.js
@@ -5,10 +5,21 @@ const inputTask = document.getElementById("task-input");
 function addTask() {
   if (inputTask.value === "") {
     alert("Ajouter une tâche");
-  } else {
+  } 
+  
+  else {
     let li = document.createElement("li");
     li.textContent = inputTask.value;
+
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "bg-red-500 text-white px-2 py-1 rounded-md hover:bg-red-600";
+    deleteButton.textContent = "Supprimer";
+    deleteButton.onclick = function() {
+      taskList.removeChild(li);
+    };
+
     taskList.appendChild(li);
+    li.appendChild(deleteButton);
     inputTask.value = ""; 
   }
 }
